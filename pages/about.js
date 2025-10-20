2d130ed98fac405ebaf0be3bc8bb11e79eea8683
allenhalsay <121465452+allenhalsay@users.noreply.github.com>
2025-10-20 12:07:25 +0000

    Adicionando página inicial e sobre

---
+++ pages/about.js
@@ -0,0 +1,4 @@
+function About(){
+    return <h1>Sobre mim: Allen Halsay</h1>
+}
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Gleyciane, eu te amo! Você me ama? Se sim, manda uma mensagem!</h1>
+}
+
+export default Home; 
